90d69ca0a1cbe1a52abda493618be68be0a5a035
ADITYA TYAGI <adityatyagiav1@gmail.com>
2023-07-21 11:56:35 +0530

    mongoose connection established and schema designed

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,8 +1,32 @@
-import React from 'react'
+"use client"
+import React, { useEffect, useState } from 'react'
 import styles from './page.module.css'
 
+
+
 const Dashboard = () => {
+  const [data,setData]=useState([]);
+  const [err,setErr]=useState(false);
+  const [isLoading,setIsLoading]=useState(false);
+
+  useEffect(()=>{
+    async function getData() {
+      setIsLoading(true)
+      const res = await fetch('https://jsonplaceholder.typicode.com/posts')
+     
+      if (!res.ok) {
+       setErr(true)
+      }
+     const items= await res.json()
+      setData(items)
+      setIsLoading(false)
+    };
+    getData()
+  },[]);
+
+  console.log(data)
   return (
+
     <div className={styles.container}>
       Dashboard
     </div>
